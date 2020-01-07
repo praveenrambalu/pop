e58d81b94c9b7982caa93a7c87bc145bc744cb98
yagnesh277 <59388044+yagnesh277@users.noreply.github.com>
2020-01-07 15:03:04 +0530

    title change

--- form.php
+++ form.php
@@ -4,7 +4,7 @@ $email=$_POST["email"];
 // $phone=$_POST["phone"];
 $message=$_POST["message"];
 $to="praveenrambalu@gmail.com"; // Receiver Email ID, Replace with your email ID
-			            	$subject='Contact Form Submit from Total fitness';
+			            	$subject='Contact Form Submit from Point One Promotion';
 						
                             $headers="From: ".$email;
 							 
@@ -19,25 +19,3 @@ $to="praveenrambalu@gmail.com"; // Receiver Email ID, Replace with your email ID
 								
 							}
 ?>
-$(function () {
-      
-    $('#contactform').on('submit', function (e) {
-
-      e.preventDefault();
-
-      $.ajax({
-        type: 'post',
-        url: 'form.php',
-        data: $('#contactform').serialize(),
-        success: function (data) {
-          if (data=="success") {
-              swal("Good job!", "We will contact you asap !", "success");
-          } else {
-              swal ( "Oops" ,  "Something went wrong!" ,  "error" )
-          }
-        }
-      });
-//   alert("hi");
-    });
-
-  });
