e1631311be06f8af63b272eb1290c604e9b98e06
yagnesh277 <59388044+yagnesh277@users.noreply.github.com>
2020-01-08 15:31:52 +0530

    mail change

--- form.php
+++ form.php
@@ -3,7 +3,7 @@ $name=$_POST["name"];
 $email=$_POST["email"];
 // $phone=$_POST["phone"];
 $message=$_POST["message"];
-$to="praveenrambalu@gmail.com"; // Receiver Email ID, Replace with your email ID
+$to="vijay@pointonepromotions.com"; // Receiver Email ID, Replace with your email ID
 			            	$subject='Contact Form Submit from Point One Promotions';
 						
                             $headers="From: ".$email;
